0d8f602fdea3f8c60f9d12db063dddffe8af732f
iosman456 <muhammetcanaltan41@gmail.com>
2025-03-03 18:01:14 +0300

    Update bruteweb.py

--- bruteweb.py
+++ bruteweb.py
@@ -9,9 +9,6 @@ import random
 import string
 import whois
 import json
-import tweepy
-import instaloader
-from TikTokApi import TikTokApi
 
 logging.basicConfig(filename='bruteforce_success.log', level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
@@ -95,74 +92,12 @@ def save_to_json(domain, domain_info, server_info):
         json.dump(data, json_file, indent=2)
 
 
-def get_twitter_info(username):
-    consumer_key = input("Enter your Twitter consumer key: ")
-    consumer_secret = input("Enter your Twitter consumer secret: ")
-    access_token = input("Enter your Twitter access token: ")
-    access_token_secret = input("Enter your Twitter access token secret: ")
-
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    
-    api = tweepy.API(auth)
-    
-    try:
-        user = api.get_user(screen_name=username)
-        tweets = api.user_timeline(screen_name=username, count=10)
-        user_info = {
-            'name': user.name,
-            'description': user.description,
-            'location': user.location,
-            'followers_count': user.followers_count,
-            'tweets': [tweet.text for tweet in tweets]
-        }
-        return user_info
-    except Exception as e:
-        return str(e)
-
-
-def get_instagram_info(username):
-    loader = instaloader.Instaloader()
-    try:
-        profile = instaloader.Profile.from_username(loader.context, username)
-        user_info = {
-            'name': profile.full_name,
-            'followers': profile.followers,
-            'following': profile.followees,
-            'posts': profile.mediacount,
-            'biography': profile.biography
-        }
-        return user_info
-    except Exception as e:
-        return str(e)
-
-
-def get_tiktok_info(username):
-    api_key = input("Enter your TikTok API key: ")
-    api = TikTokApi(api_key)
-    try:
-        user = api.get_user(username)
-        user_info = {
-            'unique_id': user['uniqueId'],
-            'nickname': user['nickname'],
-            'signature': user['signature'],
-            'followers': user['followerCount'],
-            'following': user['followingCount'],
-            'likes': user['heart'],
-            'videos': user['videoCount']
-        }
-        return user_info
-    except Exception as e:
-        return str(e)
-
-
 def main():
     print("Choose an option:")
     print("1. Brute Force")
     print("2. Web")
-    print("3. Social OSINT")
 
-    choice = input("Enter your choice (1, 2 or 3): ")
+    choice = input("Enter your choice (1 or 2): ")
 
     if choice == '1':
         domain, username = prompt_for_input()
@@ -187,22 +122,8 @@ def main():
         else:
             print(f"{domain}: Unable to find IP address")
 
-    elif choice == '3':
-        platform = input("Choose a platform (Twitter, Instagram, TikTok): ").lower()
-        username = input("Please enter a username: ")
-        if platform == 'twitter':
-            info = get_twitter_info(username)
-        elif platform == 'instagram':
-            info = get_instagram_info(username)
-        elif platform == 'tiktok':
-            info = get_tiktok_info(username)
-        else:
-            print("Invalid platform choice.")
-            sys.exit(1)
-        print(f"{platform.capitalize()} Info: {json.dumps(info, indent=2)}")
-    
     else:
-        print("Invalid choice. Please enter 1, 2 or 3.")
+        print("Invalid choice. Please enter 1 or 2.")
         sys.exit(1)
 
 
